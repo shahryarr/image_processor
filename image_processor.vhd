521e1bff1fc6f2a68278176e292cb5252bd69f31
shahryarr <78295589+shahryarr@users.noreply.github.com>
2021-02-05 12:42:34 +0330

    Add files via upload

--- image_processor.vhd
+++ image_processor.vhd
@@ -26,26 +26,23 @@ entity image_processor is
            input : in  STD_LOGIC_VECTOR (7 downto 0);
            row : in  STD_LOGIC_VECTOR (N-1 downto 0);
            column : in  STD_LOGIC_VECTOR (N-1 downto 0);
-			  done_tick : out STD_LOGIC;
            output : out  STD_LOGIC_VECTOR (7 downto 0));
 end image_processor;
 ----------------------------------------------------------------------------------
 architecture arch of image_processor is
 
-type pixel_type is array (0 to 2**N-1 , 0 to 2**N-1) of unsigned(7 downto 0);
+type pixel_type is array (0 to 2**N-1 , 0 to 2**N-1) of unsigned(8 downto 0);
 
 signal pixel : pixel_type := (others => (others => (others => '0' )));
 
-type filtered_type is array (1 to 2**N-2 , 1 to 2**N-2) of unsigned(7 downto 0);
+signal filtered : unsigned(17 downto 0);
 
-signal filtered : pixel_type := (others => (others => (others => '0' )));
-
-type mask_type  is array (0 to 2 , 0 to 2) of unsigned(7 downto 0);
+type mask_type  is array (0 to 2 , 0 to 2) of unsigned(8 downto 0);
 
 constant mask : mask_type := (
-	(to_unsigned( 0, 8),to_unsigned( 1, 8),to_unsigned( 0, 8)),
-	(to_unsigned( 1, 8),to_unsigned( 4, 8),to_unsigned( 1, 8)),
-	(to_unsigned( 0, 8),to_unsigned( 1, 8),to_unsigned( 0, 8)));
+	(to_unsigned( 0, 9),to_unsigned( 1, 9),to_unsigned( 0, 9)),
+	(to_unsigned( 1, 9),to_unsigned( 4, 9),to_unsigned( 1, 9)),
+	(to_unsigned( 0, 9),to_unsigned( 1, 9),to_unsigned( 0, 9)));
 
 type state_type is (idle , waite , op , done);
 
@@ -53,7 +50,7 @@ signal state_reg , state_next : state_type;
 
 signal row_reg , row_next : unsigned(N-1 downto 0);
 signal column_reg , column_next : unsigned(N-1 downto 0);
-signal pos , neg : unsigned(N-1 downto 0);
+signal pos , neg : unsigned(17 downto 0);
 signal i , j : integer := 1;
 ----------------------------------------------------------------------------------
 begin
@@ -67,14 +64,11 @@ begin
 			state_reg <= state_next;
 			row_reg <= row_next;
 			column_reg <= column_next;
-			pos <= pixel(i,j)*mask(1,1);
-			neg <= pixel(i-1,j)*mask(0,1) + pixel(i,j-1)*mask(1,0) + pixel(i+1,j)*mask(2,1) + pixel(i,j+1)*mask(1,2);
 		end if;
 	end process;
 --------------------------------------------------------------
 	process(state_reg , row_reg , column_reg , start , input , pos , neg)
 	begin
-		done_tick <= '0';
 		state_next <= state_reg;
 		row_next <= row_reg;
 		column_next <= column_reg;
@@ -86,12 +80,11 @@ begin
 					state_next <= waite;
 					row_next <= (others => '0');
 					column_next <= (others => '0');
-					
 				end if;
 		---------------------------------------
 			when waite =>
-				pixel(to_integer(row_reg) , to_integer(column_reg)) <= unsigned(input);
-				if (column_reg = 2 and row_reg = 2) then
+				pixel(to_integer(row_reg) , to_integer(column_reg)) <= '0' & unsigned(input);
+				if (column_reg = 2 and row_reg = 3) then
 					state_next <= op;
 					row_next <= row_reg + 1;
 				elsif (row_reg = unsigned(row)-1) then
@@ -102,16 +95,17 @@ begin
 				end if;
 		---------------------------------------
 			when op =>
-				pixel(to_integer(row_reg) , to_integer(column_reg)) <= unsigned(input);
-				
+				pixel(to_integer(row_reg) , to_integer(column_reg)) <= '0' & unsigned(input);
+				pos <= pixel(i,j)*mask(1,1);
+				neg <= pixel(i-1,j)*mask(0,1) + pixel(i,j-1)*mask(1,0) + pixel(i+1,j)*mask(2,1) + pixel(i,j+1)*mask(1,2);
 ------------filtered(i,j) <= pixel(i,j)*mask(1,1) - pixel(i-1,j)*mask(0,1) - pixel(i,j-1)*mask(1,0) - pixel(i+1,j)*mask(2,1) - pixel(i,j+1)*mask(1,2);
 			
 				if (pos < neg) then
-					filtered(i,j) <= to_unsigned(0,8);
+					filtered <= to_unsigned(0,18);
 				elsif ((pos - neg) > 255) then
-					filtered(i,j) <= to_unsigned(255,8);
+					filtered <= to_unsigned(255,18);
 				else
-					filtered(i,j) <= pos - neg;
+					filtered <= pos - neg;
 				end if;
 				
 				if (j = to_integer(unsigned(column)-2) and i = to_integer(unsigned(row)-2)) then
@@ -125,6 +119,7 @@ begin
 
 				if (column_reg = unsigned(column)-1 and row_reg = unsigned(row)-1) then
 					column_next <= (others => '0');
+					row_next <= (others => '0');
 				elsif (row_reg = unsigned(row)-1) then
 					row_next <= (others => '0');
 					column_next <= column_reg + 1;
@@ -134,14 +129,19 @@ begin
 			
 		---------------------------------------		
 			when done =>
-				done_tick <=  '1';
 				state_next <= idle;
+				if (pos < neg) then
+					filtered <= to_unsigned(0,18);
+				elsif ((pos - neg) > 255) then
+					filtered <= to_unsigned(255,18);
+				else
+					filtered <= pos - neg;
+				end if;
 				
 		end case;
 	end process;
 --------------------------------------------------------------
 
-output <= std_logic_vector(filtered(i , j));
-	
+	output <= std_logic_vector(filtered(7 downto 0));
 end arch;
 
